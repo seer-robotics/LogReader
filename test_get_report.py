ae0106e27951e1314fd532c062d6815cb650df9d
Qiangsheng <huangqiangsheng9@gmail.com>
2019-01-02 20:41:57 +0800

    精简test_get_report.py

--- test_get_report.py
+++ test_get_report.py
@@ -1,33 +1,39 @@
 from loglib import MCLoc, IMU, Odometer, Send, Get, Laser, ErrorLine, WarningLine, ReadLog, FatalLine, NoticeLine
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider,RadioButtons
 import sys
-mcl = MCLoc()
-imu = IMU()
-odo = Odometer()
-send = Send()
-get = Get()
-laser = Laser(1000.0)
-err = ErrorLine()
-war = WarningLine()
-fat = FatalLine()
-notice = NoticeLine()
-log = ReadLog(sys.argv[1:])
-log.parse(mcl, imu, odo, send, get, laser, err, war, fat, notice)
-
-f = open("Report.txt", "w", encoding='utf-8') 
-print("Files: ", sys.argv[1:], file = f)
-print(len(err.content()[0]), " ERRORs, ", len(war.content()[0]), " WARNINGs, ", len(fat.content()[0]), " FATALs, ", len(notice.content()[0]), " NOTICEs", file = f)
-print("ERRORs:", file = f)
-for data in err.content()[0]:
-    print(data,file = f)
-print("WARNINGs:", file = f)
-for data in war.content()[0]:
-    print(data, file = f)
-print("FATALs:", file = f)
-for data in fat.content()[0]:
-    print(data, file = f)
-print("NOTICEs:", file = f)
-for data in notice.content()[0]:
-    print(data, file = f)
-f.close()
+from os import listdir
+from os.path import isfile, join
+filenames = []
+if len(sys.argv) > 1:
+    filenames = sys.argv[1:]
+else:
+    mypath = "diagnosis\\log"
+    filenames = [join(mypath,f) for f in listdir(mypath) if isfile(join(mypath, f))]
+fid = open("Report.txt", "w", encoding='utf-8') 
+out = sys.stdout
+outlists = [fid,out]
+for filename in filenames:
+    log = ReadLog([filename])
+    err = ErrorLine()
+    war = WarningLine()
+    fat = FatalLine()
+    notice = NoticeLine()
+    log.parse(err, war, fat, notice)
+    for f in outlists:
+        print("="*20, file = f)
+        print("Files: ", filename, file = f)
+        print(len(err.content()[0]), " ERRORs, ", len(war.content()[0]), " WARNINGs, ", len(fat.content()[0]), " FATALs, ", len(notice.content()[0]), " NOTICEs", file = f)
+    print("ERRORs:", file = fid)
+    for data in err.content()[0]:
+        print(data,file = fid)
+    print("WARNINGs:", file = fid)
+    for data in war.content()[0]:
+        print(data, file = fid)
+    print("FATALs:", file = fid)
+    for data in fat.content()[0]:
+        print(data, file = fid)
+    print("NOTICEs:", file = fid)
+    for data in notice.content()[0]:
+        print(data, file = fid)
+fid.close()
+print("FINISHED!!!")
+ch = sys.stdin.read(1)

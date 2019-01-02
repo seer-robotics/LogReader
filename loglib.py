d92e3ecc919ee9d4212688b5fd3876cfd6124da2
Qiangsheng <huangqiangsheng9@gmail.com>
2019-01-02 15:30:37 +0800

    Modify: 适配gui，修改接口，和gui代码

--- loglib.py
+++ loglib.py
@@ -66,13 +66,13 @@ class MCLoc:
     def t(self):
         return self.data[0]
     def x(self):
-        return self.data[1]
+        return self.data[1], self.data[0]
     def y(self):
-        return self.data[2]
+        return self.data[2], self.data[0]
     def theta(self):
-        return self.data[3]
+        return self.data[3], self.data[0]
     def confidence(self):
-        return self.data[4]
+        return self.data[4], self.data[0]
 
 class IMU:
     """  陀螺仪数据
@@ -116,27 +116,27 @@ class IMU:
     def t(self):
         return self.data[0]
     def yaw(self):
-        return self.data[1]
+        return self.data[1], self.data[0]
     def ts(self):
-        return self.data[2]
+        return self.data[2], self.data[0]
     def ax(self):
-        return self.data[3]
+        return self.data[3], self.data[0]
     def ay(self):
-        return self.data[4]
+        return self.data[4], self.data[0]
     def az(self):
-        return self.data[5]
+        return self.data[5], self.data[0]
     def gx(self):
-        return self.data[6]
+        return self.data[6], self.data[0]
     def gy(self):
-        return self.data[7]
+        return self.data[7], self.data[0]
     def gz(self):
-        return self.data[8]
+        return self.data[8], self.data[0]
     def offx(self):
-        return self.data[9]
+        return self.data[9], self.data[0]
     def offy(self):
-        return self.data[10]
+        return self.data[10], self.data[0]
     def offz(self):
-        return self.data[11]
+        return self.data[11], self.data[0]
 
 class Odometer:
     """  里程数据
@@ -173,21 +173,21 @@ class Odometer:
     def ts(self):
         return self.data[1]
     def x(self):
-        return self.data[2]
+        return self.data[2], self.data[0]
     def y(self):
-        return self.data[3]
+        return self.data[3], self.data[0]
     def theta(self):
-        return self.data[4]
+        return self.data[4], self.data[0]
     def stop(self):
-        return self.data[5]
+        return self.data[5], self.data[0]
     def vx(self):
-        return self.data[6]
+        return self.data[6], self.data[0]
     def vy(self):
-        return self.data[7]
+        return self.data[7], self.data[0]
     def vw(self):
-        return self.data[8]
+        return self.data[8], self.data[0]
     def steer_angle(self):
-        return self.data[9]
+        return self.data[9], self.data[0]
 
 class Send:
     """  发送的速度数据
@@ -216,17 +216,17 @@ class Send:
     def t(self):
         return self.data[0]
     def vx(self):
-        return self.data[1]
+        return self.data[1], self.data[0]
     def vy(self):
-        return self.data[2]
+        return self.data[2], self.data[0]
     def vw(self):
-        return self.data[3]
+        return self.data[3], self.data[0]
     def steer_angle(self):
-        return self.data[4]
+        return self.data[4], self.data[0]
     def max_vx(self):
-        return self.data[5]
+        return self.data[5], self.data[0]
     def max_vw(self):
-        return self.data[6]
+        return self.data[6], self.data[0]
 
 class Get:
     """  接收的速度数据
@@ -255,17 +255,17 @@ class Get:
     def t(self):
         return self.data[0]
     def vx(self):
-        return self.data[1]
+        return self.data[1], self.data[0]
     def vy(self):
-        return self.data[2]
+        return self.data[2], self.data[0]
     def vw(self):
-        return self.data[3]
+        return self.data[3], self.data[0]
     def steer_angle(self):
-        return self.data[4]
+        return self.data[4], self.data[0]
     def max_vx(self):
-        return self.data[5]
+        return self.data[5], self.data[0]
     def max_vw(self):
-        return self.data[6]
+        return self.data[6], self.data[0]
 
 class Laser:
     """  激光雷达的数据
@@ -311,13 +311,13 @@ class Laser:
     def ts(self):
         return self.data[1]
     def angle(self):
-        return self.data[2]
+        return self.data[2], self.data[0]
     def dist(self):
-        return self.data[3]
+        return self.data[3], self.data[0]
     def x(self):
-        return self.data[4]
+        return self.data[4], self.data[0]
     def y(self):
-        return self.data[5]
+        return self.data[5], self.data[0]
 
 class ErrorLine:
     """  错误信息
@@ -335,7 +335,7 @@ class ErrorLine:
     def t(self):
         return self.data[0]
     def content(self):
-        return self.data[1]
+        return self.data[1], self.data[0]
 
 class WarningLine:
     """  报警信息
@@ -353,7 +353,7 @@ class WarningLine:
     def t(self):
         return self.data[0]
     def content(self):
-        return self.data[1]
+        return self.data[1], self.data[0]
 
 class FatalLine:
     """  错误信息
@@ -371,7 +371,7 @@ class FatalLine:
     def t(self):
         return self.data[0]
     def content(self):
-        return self.data[1]
+        return self.data[1], self.data[0]
 
 class NoticeLine:
     """  注意信息
@@ -389,7 +389,7 @@ class NoticeLine:
     def t(self):
         return self.data[0]
     def content(self):
-        return self.data[1]
+        return self.data[1], self.data[0]
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -402,7 +402,7 @@ if __name__ == '__main__':
     laser = Laser(1000.0)
     err = ErrorLine()
     war = WarningLine()
-    log = ReadLog("test.log")
+    log = ReadLog(["test1.log"])
     log.parse(mcl, imu, odo, send, get, laser, err, war)
 
     print(len(err.content()), " ERRORs:")
@@ -413,114 +413,114 @@ if __name__ == '__main__':
     plt.figure(1)
     plt.subplot(4,1,1)
     plt.title('MCLoc')
-    plt.plot(mcl.t(), mcl.x(),'.', label = 'x')
+    plt.plot(mcl.x()[1], mcl.x()[0],'.', label = 'x')
     plt.legend()
     plt.subplot(4,1,2)
-    plt.plot(mcl.t(), mcl.y(),'.', label = 'y')
+    plt.plot(mcl.y()[1], mcl.y()[0],'.', label = 'y')
     plt.legend()
     plt.subplot(4,1,3)
-    plt.plot(mcl.t(), mcl.theta(),'.', label = 'theta')
+    plt.plot(mcl.theta()[1], mcl.theta()[0],'.', label = 'theta')
     plt.legend()
     plt.subplot(4,1,4)
-    plt.plot(mcl.t(), mcl.confidence(),'.', label = 'confidence')
+    plt.plot(mcl.confidence()[1], mcl.confidence()[0],'.', label = 'confidence')
     plt.legend()
 
     plt.figure(21)
     plt.title('IMU Yaw')
-    plt.plot(imu.t(), imu.yaw(),'.')
+    plt.plot(imu.yaw()[1], imu.yaw()[0],'.')
     plt.figure(2)
     plt.subplot(3,3,1)
     plt.title('IMU')
-    plt.plot(imu.t(), imu.ax(),'.', label = 'ax')
+    plt.plot(imu.ax()[1], imu.ax()[0],'.', label = 'ax')
     plt.legend()
     plt.subplot(3,3,2)
-    plt.plot(imu.t(), imu.ay(),'.', label = 'ay')
+    plt.plot(imu.t(), imu.ay()[0],'.', label = 'ay')
     plt.legend()
     plt.subplot(3,3,3)
-    plt.plot(imu.t(), imu.az(),'.', label = 'az')
+    plt.plot(imu.t(), imu.az()[0],'.', label = 'az')
     plt.legend()
     plt.subplot(3,3,4)
-    plt.plot(imu.t(), imu.gx(),'.', label = 'gx')
+    plt.plot(imu.t(), imu.gx()[0],'.', label = 'gx')
     plt.legend()
     plt.subplot(3,3,5)
-    plt.plot(imu.t(), imu.gy(),'.', label = 'gy')
+    plt.plot(imu.t(), imu.gy()[0],'.', label = 'gy')
     plt.legend()
     plt.subplot(3,3,6)
-    plt.plot(imu.t(), imu.gz(),'.', label = 'gz')
+    plt.plot(imu.t(), imu.gz()[0],'.', label = 'gz')
     plt.legend()
     plt.subplot(3,3,7)
-    plt.plot(imu.t(), imu.offx(),'.', label = 'offx')
+    plt.plot(imu.t(), imu.offx()[0],'.', label = 'offx')
     plt.legend()
     plt.subplot(3,3,8)
-    plt.plot(imu.t(), imu.offy(),'.', label = 'offy')
+    plt.plot(imu.t(), imu.offy()[0],'.', label = 'offy')
     plt.legend()
     plt.subplot(3,3,9)
-    plt.plot(imu.t(), imu.offz(),'.', label = 'offz')
+    plt.plot(imu.t(), imu.offz()[0],'.', label = 'offz')
     plt.legend()
 
     plt.figure(3)
     plt.subplot(2,3,1)
     plt.title('Odometer')
-    plt.plot(odo.t(), odo.x(),'.', label = 'x')
+    plt.plot(odo.t(), odo.x()[0],'.', label = 'x')
     plt.legend()
     plt.subplot(2,3,2)
-    plt.plot(odo.t(), odo.y(),'.', label = 'y')
+    plt.plot(odo.t(), odo.y()[0],'.', label = 'y')
     plt.legend()
     plt.subplot(2,3,3)
-    plt.plot(odo.t(), odo.theta(),'.', label = 'theta')
+    plt.plot(odo.t(), odo.theta()[0],'.', label = 'theta')
     plt.legend()
     plt.subplot(2,3,4)
-    plt.plot(odo.t(), odo.vx(),'.', label = 'vx')
+    plt.plot(odo.t(), odo.vx()[0],'.', label = 'vx')
     plt.legend()
     plt.subplot(2,3,5)
-    plt.plot(odo.t(), odo.vy(),'.', label = 'vy')
+    plt.plot(odo.t(), odo.vy()[0],'.', label = 'vy')
     plt.legend()
     plt.subplot(2,3,6)
-    plt.plot(odo.t(), odo.vw(),'.', label = 'vw')
+    plt.plot(odo.t(), odo.vw()[0],'.', label = 'vw')
     plt.legend()
 
     plt.figure(4)
     plt.subplot(2,2,1)
     plt.title('Send And Get Velocity')
-    plt.plot(send.t(), send.vx(), 'o', label= 'send vx')
-    plt.plot(get.t(), get.vx(), '.', label= 'get vx')
-    plt.plot(send.t(), send.max_vx(), 'o', label= 'send max vx')
-    plt.plot(get.t(), get.max_vx(), '.', label= 'get max vx')
+    plt.plot(send.t(), send.vx()[0], 'o', label= 'send vx')
+    plt.plot(get.t(), get.vx()[0], '.', label= 'get vx')
+    plt.plot(send.t(), send.max_vx()[0], 'o', label= 'send max vx')
+    plt.plot(get.t(), get.max_vx()[0], '.', label= 'get max vx')
     plt.legend()
     plt.subplot(2,2,2)
-    plt.plot(send.t(), send.vy(), 'o', label= 'send vy')
-    plt.plot(get.t(), get.vy(), '.', label= 'get vy')
+    plt.plot(send.t(), send.vy()[0], 'o', label= 'send vy')
+    plt.plot(get.t(), get.vy()[0], '.', label= 'get vy')
     plt.legend()
     plt.subplot(2,2,3)
-    plt.plot(send.t(), send.vw(), 'o', label= 'send vw')
-    plt.plot(get.t(), get.vw(), '.', label= 'get vw')
-    plt.plot(send.t(), send.max_vw(), 'o', label= 'send max vw')
-    plt.plot(get.t(), get.max_vw(), '.', label= 'get max vw')
+    plt.plot(send.t(), send.vw()[0], 'o', label= 'send vw')
+    plt.plot(get.t(), get.vw()[0], '.', label= 'get vw')
+    plt.plot(send.t(), send.max_vw()[0], 'o', label= 'send max vw')
+    plt.plot(get.t(), get.max_vw()[0], '.', label= 'get max vw')
     plt.legend()
     plt.subplot(2,2,4)
-    plt.plot(send.t(), send.steer_angle(), 'o', label= 'send steer_angle')
-    plt.plot(get.t(), get.steer_angle(), '.', label= 'get steer_angle')
+    plt.plot(send.t(), send.steer_angle()[0], 'o', label= 'send steer_angle')
+    plt.plot(get.t(), get.steer_angle()[0], '.', label= 'get steer_angle')
     plt.legend()
 
     plt.figure(5)
     plt.subplot(2,1,1)
     plt.title("Laser")
     plt.subplots_adjust(bottom=0.2,left=0.1) 
-    l1, = plt.plot(laser.x()[1], laser.y()[1], '.')
+    l1, = plt.plot(laser.x()[0][0], laser.y()[0][0], '.')
     plt.axis('equal')
     plt.grid()
     plt.subplot(2,1,2,projection = 'polar')
     plt.subplots_adjust(bottom=0.2,left=0.1) 
-    l2, = plt.plot(laser.angle()[1], laser.dist()[1], '.')
+    l2, = plt.plot(laser.angle()[0][0], laser.dist()[0][0], '.')
     axcolor = 'lightgoldenrodyellow'  # slider的颜色
     om1= plt.axes([0.1, 0.08, 0.8, 0.02], facecolor=axcolor) # 第一slider的位置
     som1 = Slider(om1, r'Time', 0, len(laser.ts())-1, valinit=0, valfmt='%i') #产生第二slider
     print(len(laser.ts()))
     def update(val):
         s1 = int(som1.val)
-        l1.set_xdata(laser.x()[s1])
-        l1.set_ydata(laser.y()[s1])
-        l2.set_xdata(laser.angle()[s1])
-        l2.set_ydata(laser.dist()[s1])
+        l1.set_xdata(laser.x()[0][s1])
+        l1.set_ydata(laser.y()[0][s1])
+        l2.set_xdata(laser.angle()[0][s1])
+        l2.set_ydata(laser.dist()[0][s1])
     som1.on_changed(update)
     plt.show()
--- test_gui.py
+++ test_gui.py
@@ -1,8 +1,10 @@
-from loglib import MCLoc, IMU, Odometer, Send, Get, Laser, ErrorLine, WarningLine, ReadLog, FatalLine, AlarmLine
+from loglib import MCLoc, IMU, Odometer, Send, Get, Laser, ErrorLine, WarningLine, ReadLog, FatalLine, NoticeLine
 from loglib import findrange
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider,Button
+from matplotlib.widgets import Slider,Button, TextBox
 import sys
+from datetime import datetime, timedelta
+
 mcl = MCLoc()
 imu = IMU()
 odo = Odometer()
@@ -12,48 +14,68 @@ laser = Laser(1000.0)
 err = ErrorLine()
 war = WarningLine()
 fatal = FatalLine()
-alarm = AlarmLine()
-# for file in files:
-# #    log = ReadLog("log/robokit_2018-12-27_11-05-54.87.log")
-#     log = ReadLog(file)
-#     log.parse(mcl, imu, odo, send, get, laser, err, war)
-#     print(file,":")
-#     print(max(imu.offx()) - min(imu.offx()), " ", max(imu.offy()) - min(imu.offy()), " ", max(imu.offz()) - min(imu.offz()))
-log = ReadLog(sys.argv[1:])
-log.parse(mcl, imu, odo, send, get, laser, err, war,fatal, alarm)
+notice = NoticeLine()
+log = ReadLog(['log\\robokit_2018-12-26_22-38-10.41.log'])
+log.parse(mcl, imu, odo, send, get, laser, err, war,fatal, notice)
+
+tmax = max(mcl.t() + odo.t() + send.t() + get.t() + laser.t() + err.t() + fatal.t() + notice.t())
+tmin = min(mcl.t() + odo.t() + send.t() + get.t() + laser.t() + err.t() + fatal.t() + notice.t())
+dt = tmax - tmin
+tlist = [tmin + timedelta(microseconds=x) for x in range(0, int(dt.total_seconds()*1e6+1000),1000)]
 
 f = open("Report.txt", "w", encoding='utf-8') 
-print(len(fatal.content()), "FATALs", len(err.content()), " ERRORs, ", len(war.content()), " WARNINGs", len(alarm.content()), "ALARMs", file = f )
+print(len(fatal.content()[0]), "FATALs", len(err.content()[0]), " ERRORs, ", len(war.content()[0]), " WARNINGs", len(notice.content()[0]), "NOTICEs", file = f )
 print("FATALs:", file = f)
-for data in fatal.content():
+for data in fatal.content()[0]:
     print(data,file = f)
 print("ERRORs:", file = f)
-for data in err.content():
+for data in err.content()[0]:
     print(data,file = f)
 print("WARNINGs:", file = f)
-for data in war.content():
+for data in war.content()[0]:
     print(data, file = f)
-print("ALARMs:", file = f)
-for data in alarm.content():
+print("NOTICEs:", file = f)
+for data in notice.content()[0]:
     print(data, file = f)
 f.close()
+
+def drawFEWN(ax):
+    """ 绘制 Fatal, Error, Warning在坐标轴上"""
+    fl, el, wl,nl = None, None, None, None
+    for tmp in fatal.t():
+        fl, = ax.plot((tmp,tmp),[-1e10, 1e10],'k-')
+    for tmp in err.t():
+        el, = ax.plot((tmp,tmp),[-1e10, 1e10],'r-.')
+    for tmp in war.t():
+        wl, = ax.plot((tmp,tmp),[-1e10, 1e10],'y--')
+    for tmp in notice.t():
+        nl, = ax.plot((tmp,tmp),[-1e10, 1e10],'g:')
+    ax.legend((fl, el, wl, nl), ('fatal', 'error', 'warning', 'notice'), loc='upper right')
 
 
 if len(mcl.t()) > 0:
     fig = plt.figure(1)
-    ax = plt.axes([0.1, 0.2, 0.8, 0.3])
+    ax2 = plt.axes([0.1, 0.2, 0.8, 0.3])
     #画图 mcl.t, mcl.x
-    l1, = plt.plot(mcl.t(), mcl.x(), '.')
-    plt.grid()
-    ax_xy = plt.axes([0.1, 0.55, 0.8, 0.3])
+    drawFEWN(ax2)
+    l2, = ax2.plot(mcl.y()[1], mcl.y()[0], '.')
+    max_range = max(mcl.y()[0]) - min(mcl.y()[0])
+    ax2.set_ylim(min(mcl.y()[0]) - 0.05 * max_range, max(mcl.y()[0]) + 0.05 * max_range)
+    ax2.set_xlabel("t")
+    ax2.grid()
     #画图 mcl.t, mcl.x
-    l_xy, = plt.plot(mcl.x(), mcl.y(), '.')
-    plt.grid()
+    ax1 = plt.axes([0.1, 0.58, 0.8, 0.3])
+    drawFEWN(ax1)
+    l1, = ax1.plot(mcl.x()[1], mcl.x()[0], '.')
+    max_range = max(mcl.x()[0]) - min(mcl.x()[0])
+    ax1.set_ylim(min(mcl.x()[0]) - 0.05 * max_range, max(mcl.x()[0]) + 0.05 * max_range)
+    ax1.set_xticklabels("")
+    ax1.grid()
 
     #slider
     axcolor = 'lightgoldenrodyellow'  # slider的颜色
     om1= plt.axes([0.1, 0.1, 0.8, 0.05], facecolor=axcolor) # 第一slider的位置
-    som1 = Slider(om1, r'Start',0, len(mcl.t())-1, valinit=0) #产生第一slider
+    som1 = Slider(om1, r'Start',0, len(tlist)-1, valinit=0) #产生第一slider
     # slider的val text 重写
     som1.valtext.set_text("")
     som1.valtext = om1.text(0.02, 0.5, mcl.t()[0].strftime("%H:%M:%S.%f"),
@@ -62,97 +84,81 @@ if len(mcl.t()) > 0:
                         horizontalalignment='left')
     som1.valtext.set_text(mcl.t()[0].strftime("%H:%M:%S.%f"))
     om2= plt.axes([0.1, 0.02, 0.8, 0.05], facecolor=axcolor) # 第一slider的位置
-    som2 = Slider(om2, r'End', 1, len(mcl.t())-1, valinit=len(mcl.t())-1) #产生第一slider
+    som2 = Slider(om2, r'End', 1, len(tlist)-1, valinit=len(tlist)-1) #产生第一slider
     # slider的val text 重写
     som2.valtext.set_text("")
     som2.valtext = om2.text(0.02, 0.5, mcl.t()[-1].strftime("%H:%M:%S.%f"),
                         transform=om2.transAxes,
                         verticalalignment='center',
                         horizontalalignment='left')
-    # Button
-    tx = 0.0
-    tdx = 0.03
-    mcl_x_ax = plt.axes([tx, 0.9, 0.03, 0.03])
-    mcl_x_bt = Button(mcl_x_ax, 'x')
-    mcl_y_ax = plt.axes([tx+tdx, 0.9, 0.03, 0.03])
-    mcl_y_bt = Button(mcl_y_ax, 'y')
-    mcl_theta_ax = plt.axes([tx+2*tdx, 0.9, 0.03, 0.03])
-    mcl_theta_bt = Button(mcl_theta_ax, r"$\theta$")
-    mcl_confidence_ax = plt.axes([tx+3*tdx, 0.9, 0.03, 0.03])
-    mcl_confidence_bt = Button(mcl_confidence_ax, r"C")
-
+    # Text input
+    box1 = plt.axes([0.1, 0.9, 0.8, 0.04])
+    text_box1 = TextBox(box1, 'Evaluate', initial="mcl.x")
+    ax1.set_ylabel("mcl.x")
+    box2 = plt.axes([0.1, 0.52, 0.8, 0.04])
+    text_box2 = TextBox(box2, 'Evaluate', initial="mcl.y")
+    ax2.set_ylabel("mcl.y")
     class Update:
-        def __init__(self,t,data,x,y):
-            self.ind1 = 0
-            self.ind2 = len(t)-1 
+        def __init__(self, t, t1, data1, t2, data2):
             self.t = t
+            self.inds = 0
+            self.inde = len(t)-1 
             self.ts = t[0]
             self.te = t[-1]
-            self.data = data
-            self.x = x
-            self.y = y 
+
+            self.t1 = t1
+            self.data1 = data1
+            self.t2 = t2
+            self.data2 = data2
         def slider_start(self, event):
-            print("slider start event = ",event)
-            self.ind1 = int(som1.val)
-            if self.ind1 >= self.ind2 :
-                self.ind1 = self.ind2 - 1
-                som1.set_val(self.ind1)
-            self.ts = self.t[self.ind1]
-            som1.valtext.set_text(self.t[self.ind1].strftime("%H:%M:%S.%f"))
-            ax.set_xlim(self.t[self.ind1], self.t[self.ind2])
-            l_xy.set_xdata(self.x[self.ind1:self.ind2])
-            l_xy.set_ydata(self.y[self.ind1:self.ind2])
+            self.inds = int(som1.val)
+            if self.inds >= self.inde :
+                self.inds = self.inde - 1
+                som1.set_val(self.inds)
+            self.ts = self.t[self.inds]
+            som1.valtext.set_text(self.ts.strftime("%H:%M:%S.%f"))
+            ax1.set_xlim(self.ts, self.te)
+            ax2.set_xlim(self.ts, self.te)
+            plt.draw()
         def slider_end(self, event):
-            print("slider end event = ",event)
-            self.ind2 = int(som2.val)
-            if self.ind1 >= self.ind2 :
-                self.ind2 = self.ind1 + 1
-                som2.set_val(self.ind2)
-            self.te = self.t[self.ind2]
-            som2.valtext.set_text(self.t[self.ind2].strftime("%H:%M:%S.%f"))
-            ax.set_xlim(self.t[self.ind1], self.t[self.ind2])
-            l_xy.set_xdata(self.x[self.ind1:self.ind2])
-            l_xy.set_ydata(self.y[self.ind1:self.ind2])
-        def up_mcl_x(self, event):
-            print("button event = ",event)
-            self.t = mcl.t()
-            self.data = mcl.x()
-            self.ind1, self.ind2 = findrange(self.t, self.ts, self.te)
-            self.reset_data()
-        def up_mcl_y(self, event):
-            print("button event = ",event)
-            self.t = mcl.t()
-            self.data = mcl.y()
-            self.ind1, self.ind2 = findrange(self.t, self.ts, self.te)
-            self.reset_data()
-        def up_mcl_theta(self, event):
-            print("button event = ",event)
-            self.t = mcl.t()
-            self.data = mcl.theta()
-            self.ind1, self.ind2 = findrange(self.t, self.ts, self.te)
-            self.reset_data()
-        def up_mcl_confidence(self, event):
-            print("button event = ",event)
-            self.t = mcl.t()
-            self.data = mcl.confidence()
-            self.ind1, self.ind2 = findrange(self.t, self.ts, self.te)
-            self.reset_data()
-        def reset_data(self):
-            som1.set_val(self.ind1)
-            som2.set_val(self.ind2)
-            som1.valtext.set_text(self.t[self.ind1].strftime("%H:%M:%S.%f"))
-            som2.valtext.set_text(self.t[self.ind2].strftime("%H:%M:%S.%f"))
-            l1.set_xdata(self.t[self.ind1:self.ind2])
-            l1.set_ydata(self.data[self.ind1:self.ind2])
-            ax.set_xlim(self.t[self.ind1], self.t[self.ind2])
-            ax.set_ylim(min(self.data), max(self.data))
+            self.inde = int(som2.val)
+            if self.inds >= self.inde :
+                self.inde = self.inds + 1
+                som2.set_val(self.inde)
+            self.te = self.t[self.inde]
+            som2.valtext.set_text(self.te.strftime("%H:%M:%S.%f"))
+            ax1.set_xlim(self.ts, self.te)
+            ax2.set_xlim(self.ts, self.te)
+            plt.draw()
+        def submit1(self, event):
+            val = event.replace(' ',"")
+            self.data1, self.t1 = eval(val+"()")
+            ax1.set_ylabel(val)
+            self.reset_data1()
+        def submit2(self, event):
+            val = event.replace(' ',"")
+            self.data2, self.t2 = eval(val+"()")
+            ax2.set_ylabel(val)
+            self.reset_data2()
+        def reset_data1(self):
+            l1.set_xdata(self.t1)
+            l1.set_ydata(self.data1)
+            ax1.set_xlim(self.ts, self.te)
+            max_range = max(self.data1) - min(self.data1)
+            ax1.set_ylim(min(self.data1) - 0.05 * max_range, max(self.data1) + 0.05 * max_range)
+            plt.draw()
+        def reset_data2(self):
+            l2.set_xdata(self.t2)
+            l2.set_ydata(self.data2)
+            ax2.set_xlim(self.ts, self.te)
+            max_range = max(self.data2) - min(self.data2)
+            ax2.set_ylim(min(self.data2) - 0.05 * max_range, max(self.data2)  + 0.05 * max_range)
+            plt.draw()
 
     #CallBack
-    update = Update(mcl.t(),mcl.x(),mcl.x(),mcl.y())
+    update = Update(tlist,mcl.x()[1], mcl.x()[0], mcl.y()[1], mcl.y()[0])
     som1.on_changed(update.slider_start)
     som2.on_changed(update.slider_end)
-    mcl_x_bt.on_clicked(update.up_mcl_x)
-    mcl_y_bt.on_clicked(update.up_mcl_y)
-    mcl_theta_bt.on_clicked(update.up_mcl_theta)
-    mcl_confidence_bt.on_clicked(update.up_mcl_confidence)
+    text_box1.on_submit(update.submit1)
+    text_box2.on_submit(update.submit2)
     plt.show()
